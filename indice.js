563170e4b0f5219f6d45fb421d404bda9634fbcb
Nathália Ramos <88409702+NathyNickii@users.noreply.github.com>
2021-12-05 23:07:40 -0300

    Add files via upload

---
+++ indice.js
@@ -0,0 +1,25 @@
+let comida = ["Ovo", "Feijão", "Macarrão", "Cenoura"]
+
+let qtd_itens_comida = comida.length
+
+console.log(qtd_itens_comida)
+
+
+
+const listaNome = ["Nathália", "Fernanda", "Amanda", "Melissa"]
+
+
+console.log(listaNome)
+
+
+
+const professoras = ["Nathália", "Fernanda", "Amanda", "Melissa"]
+
+professoras.unshift("Ana")
+
+console.log(professoras)
+
+
+const listaAfazeres = ["Tomar café", "Estudar", "Almoçar", "Desconçar"]
+
+console.log(listaAfazeres)
